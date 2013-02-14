7e7ab6a5490786c30fbe7c5030ad0a6fd246bc72
Bharti <bhartin@thoughtworks.com>
2013-02-14 15:38:00 +0300

    feature toggling tests

--- healthmodels/tests/test_models.py
+++ healthmodels/tests/test_models.py
@@ -3,6 +3,7 @@ from django.core import management
 from healthmodels.models.HealthFacility import *
 from django.db import IntegrityError
 from mock import *
+import settings
 
 class TestHealthFacilityBase(TestCase):
 
@@ -18,23 +19,24 @@ class TestHealthFacilityBase(TestCase):
 
     new_facility = HealthFacilityBase(name="Dummy 1", uuid="uuid")
     self.failUnlessRaises(IntegrityError, new_facility.save, cascade_update=False)
+    
+  if settings.CASCADE_UPDATE_TO_DHIS2:
+    @patch('fred_consumer.fred_connect.FredFacilitiesFetcher.send_facility_update')
+    def test_save(self, mock_send_facility_update):
+        facility = HealthFacilityBase(name="Dummy")
+        facility.save()
+        assert mock_send_facility_update.called == True
 
-  @patch('fred_consumer.fred_connect.FredFacilitiesFetcher.send_facility_update')
-  def test_save(self, mock_send_facility_update):
-      facility = HealthFacilityBase(name="Dummy")
-      facility.save()
-      assert mock_send_facility_update.called == True
+    @patch('fred_consumer.fred_connect.FredFacilitiesFetcher.send_facility_update')
+    def test_save_without_cascade_update(self, mock_send_facility_update):
+        facility = HealthFacilityBase(name="Dummy")
+        facility.save(cascade_update=False)
+        assert mock_send_facility_update.called == False
 
-  @patch('fred_consumer.fred_connect.FredFacilitiesFetcher.send_facility_update')
-  def test_save_without_cascade_update(self, mock_send_facility_update):
-      facility = HealthFacilityBase(name="Dummy")
-      facility.save(cascade_update=False)
-      assert mock_send_facility_update.called == False
-
-  @patch('fred_consumer.fred_connect.FredFacilitiesFetcher.send_facility_update')
-  def test_unsuccesful_cascade_update_do_not_save(self, mock_send_facility_update):
-      mock_send_facility_update.return_value = False
-      facility = HealthFacilityBase(name="Dummy")
-      self.failUnlessRaises(ValidationError, facility.save, cascade_update=True)
-      self.failIf(facility.id)
+    @patch('fred_consumer.fred_connect.FredFacilitiesFetcher.send_facility_update')
+    def test_unsuccesful_cascade_update_do_not_save(self, mock_send_facility_update):
+        mock_send_facility_update.return_value = False
+        facility = HealthFacilityBase(name="Dummy")
+        self.failUnlessRaises(ValidationError, facility.save, cascade_update=True)
+        self.failIf(facility.id)
 
